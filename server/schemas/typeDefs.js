e57f6edce492f79264b54391cf3529dc11d166ff
Venus DM <crhowe90@gmail.com>
2022-10-15 14:49:59 -0500

    typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+// import the gql tagged template function
+const { gql } =require('apollo-server-express');
+// create our typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+}
+    type Book {
+        id: String
+        authors: [String!]
+        description: String
+        title: String!
+        image: String
+        link: Sting
+    }
+    type Auth{
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username:String!, email: String!, password: String!): Auth
+        saveBook(author: [String!], description: String, title: String!, bookId: String!, image: String, link: String )
+        removeBook(bookId: String!): User
+    }
+`;
+// export the typeDefs
+module.exports = typeDefs;
